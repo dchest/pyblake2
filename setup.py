3f432002b666651ec2c385c24d3030c84b389133
Dmitry Chestnykh <dmitry@codingrobots.com>
2013-04-10 17:11:07 +0200

    Remove PKG_INFO, add MANIFEST.in.
    Manage package via setup.py, not manually.

--- setup.py
+++ setup.py
@@ -1,5 +1,24 @@
 from distutils.core import setup, Extension
 
+LONG_DESC = """\
+pyblake2 is an extension module for Python implementing BLAKE2 hash function.
+        
+BLAKE2 is a cryptographic hash function, which offers highest security while
+being as fast as MD5 or SHA-1, and comes in two flavors:
+
+* BLAKE2b, optimized for 64-bit platforms and produces digests of any size
+  between 1 and 64 bytes,
+
+* BLAKE2s, optimized for 8- to 32-bit platforms and produces digests of any
+  size between 1 and 32 bytes.
+
+BLAKE2 supports keyed mode (a faster and simpler replacement for HMAC),
+salted hashing, personalization, and tree hashing.
+
+Hash objects from this module follow the API of standard library's
+`hashlib` objects.
+"""
+
 pyblake2 = Extension('pyblake2',
                    define_macros = [
                     # Which implementation to use for compression function:
@@ -20,11 +39,30 @@ pyblake2 = Extension('pyblake2',
 
 
 setup(name='pyblake2',
-      version='0.1',
-      description='BLAKE2 module for Python',
+      version='0.9.0',
+      description='BLAKE2 hash function extension module',
+      long_description=LONG_DESC,
       author='Dmitry Chestnykh',
       author_email='dmitry@codingrobots.com',
       license='http://creativecommons.org/publicdomain/zero/1.0/',
       url='https://github.com/dchest/pyblake2',
-      ext_modules=[pyblake2])
+      ext_modules=[pyblake2],
+      classifiers=[
+            'Classifier: Development Status :: 4 - Beta',
+            'Classifier: Intended Audience :: Developers',
+            'Classifier: Intended Audience :: Information Technology',
+            'Classifier: Intended Audience :: Science/Research',
+            'Classifier: License :: CC0 1.0 Universal (CC0 1.0) Public Domain Dedication',
+            'Classifier: Operating System :: OS Independent',
+            'Classifier: Programming Language :: Python :: 2',
+            'Classifier: Programming Language :: Python :: 2.6',
+            'Classifier: Programming Language :: Python :: 2.7',
+            'Classifier: Programming Language :: Python :: 3',
+            'Classifier: Programming Language :: Python :: 3.0',
+            'Classifier: Programming Language :: Python :: 3.1',
+            'Classifier: Programming Language :: Python :: 3.2',
+            'Classifier: Programming Language :: Python :: 3.3',
+            'Classifier: Topic :: Security :: Cryptography'
+          ]
+      )
 
